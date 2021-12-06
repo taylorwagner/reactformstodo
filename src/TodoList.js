dbd88411361d1dba1110cc89c8e7b53fccf218a1
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-05 23:44:45 -0600

    todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Todo from './Todo';
 import NewTodoForm from './NewTodoForm';
 
 const TodoList = () => {
+    const [todos, setNewTodos] = useState([]);
+    const add = todoObj => {
+        setNewTodos(todos => [...todos, todoObj]);
+    };
+    const remove = id => {
+        setNewTodos(todos => todos.filter(todo => todo.id !== id));
+    };
 
+    const todoComp = todos.map(todo => (
+        <Todo
+            key={todo.id}
+            id={todo.id}
+            task={todo.task}
+            handleRemove={remove}
+        />
+    ));
+
+    return (
+        <>
+            <NewTodoForm createTodo={add} />
+            {todoComp}
+        </>
+    );
 }
 
 export default TodoList;
