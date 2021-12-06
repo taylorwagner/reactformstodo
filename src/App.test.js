d19614c52fae79c2763fa36047edb61b6546b62d
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-05 23:25:47 -0600

    files set up

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,15 @@
+import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+it("renders without crashing", function() {
+  render(<App />);
+});
+
+it("matches snapshot", function() {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
 
 test('renders learn react link', () => {
   render(<App />);
