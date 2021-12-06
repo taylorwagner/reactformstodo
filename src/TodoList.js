3bcf1ed3d1b9d43fc16aef0791d738f1d9dc979a
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-06 09:06:23 -0600

    include update feature'

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,26 +4,37 @@ import NewTodoForm from './NewTodoForm';
 
 const TodoList = () => {
     const [todos, setNewTodos] = useState([]);
+
     const add = todoObj => {
         setNewTodos(todos => [...todos, todoObj]);
     };
+
+    const update = (id, updatedTask) => {
+        setTodos(todos =>
+          todos.map(todo =>
+            todo.id === id ? { ...todo, task: updatedTask } : todo
+          )
+        );
+      };
+
     const remove = id => {
         setNewTodos(todos => todos.filter(todo => todo.id !== id));
     };
 
     const todoComp = todos.map(todo => (
         <Todo
+            remove={remove}
             key={todo.id}
             id={todo.id}
             task={todo.task}
-            handleRemove={remove}
+            update={update}
         />
     ));
 
     return (
         <>
             <NewTodoForm createTodo={add} />
-            {todoComp}
+            <ul>{todoComp}</ul>
         </>
     );
 }
