bbb0141fcc59f8cbe2340d77c24d69637b02fd2e
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-06 09:04:10 -0600

    todo with CRUD

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = ({ id, task }) => {
-    const remove = () => handleRemove(id);
-    return (
+const Todo = ({ id = "1", task = "default todo", remove, update }) => {
+    const [editTask, setEditTask ] = useState(task);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const toggleEdit = () => {
+        setIsEditing(edit => !edit);
+    };
+    
+    const handleChange = (e) => {
+        setEditTask(e.target.value);
+    };
+
+    const handleDelete = () => remove(id);
+
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        update(id, editTask);
+        setIsEditing(false);
+    }
+
+    // default todo view
+    let jsx = (
         <div>
-            { task }
-            <button onClick={remove}>X</button>
+            <li>{task}</li>
+            <button onClick={toggleEdit}>Edit</button>
+            <button onClick={handleDelete}>X</button>
         </div>
-    )
+    );
+
+    // todo view when editing
+    if (isEditing) {
+        jsx = (
+            <div>
+                <form onSubmit={handleUpdate}>
+                    <input type="text" value={editTask} onChange={handleChange} />
+                    <button>Update!</button>
+                </form>
+            </div>
+        );
+    }
+
+    return jsx;
 }
 
 export default Todo;
