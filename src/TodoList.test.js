0874c0a92b94bcefe36d5f71b04c595f6f090d64
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-05 20:36:59 -0600

    snapshot and smoke tests

---
+++ src/TodoList.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import TodoList from "./TodoList";
+
+it("renders without crashing", function() {
+    render(<TodoList />);
+  });
+  
+  it("matches snapshot", function() {
+    const { asFragment } = render(<TodoList />);
+    expect(asFragment()).toMatchSnapshot();
+  });
