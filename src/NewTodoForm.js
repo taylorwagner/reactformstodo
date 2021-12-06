c134263ddf83c8341c590924f1fab285fc568984
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-06 08:34:40 -0600

    form created

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import uuid from "uuid/v4";
 
 const NewTodoForm = () => {
+    const INITIAL_STATE = {
+        task: ""
+    };
+
+    const [formData, setFormData] = useState(INITIAL_STATE);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(data => ({
+            ...data,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        newTask({ ...formData, id: uuid() });
+        setFormData(INITIAL_STATE);
+    };
+
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor={task}>Task to Complete</label>
+                    <input
+                        id="task"
+                        type="text"
+                        name="task"
+                        value={formData.task}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button id="newTodoButton">Add Todo!</button>
+            </form>
+        </>
+    )
 
 }
 
