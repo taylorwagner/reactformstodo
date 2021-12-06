0af84a5774af5593eceeae6cb3c7a2d84b9f7670
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-06 08:49:49 -0600

    simplify

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,37 +1,29 @@
 import React, { useState } from "react";
-import uuid from "uuid/v4";
+import uuid from "uuid/v1";
 
 const NewTodoForm = () => {
-    const INITIAL_STATE = {
-        task: ""
+    const [task, setTask] = useState("");
+
+    const handleChange = evt => {
+        setTask(evt.target.value);
     };
 
-    const [formData, setFormData] = useState(INITIAL_STATE);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(data => ({
-            ...data,
-            [name]: value
-        }));
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        newTask({ ...formData, id: uuid() });
-        setFormData(INITIAL_STATE);
+    const gatherInput = evt => {
+        evt.preventDefault();
+        createTodo({ task, id: uuid() });
+        setTask("");
     };
 
     return(
         <>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={gatherInput}>
                 <div>
                     <label htmlFor={task}>Task to Complete</label>
                     <input
                         id="task"
                         type="text"
                         name="task"
-                        value={formData.task}
+                        value={task}
                         onChange={handleChange}
                     />
                 </div>
