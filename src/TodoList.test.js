6718b2b0433147c8222f9a418a346dcfdd7d4394
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-06 08:39:08 -0600

    initial todo set up

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, wait } from "@testing-library/react";
 import TodoList from "./TodoList";
+
+function addTodo(todoList, task = "clean bathroom") {
+  const taskInput = todoList.getByLabelText("Task to Complete");
+  fireEvent.change(taskInput, { target: { value: task }});
+  const button = todoList.getByText("Add Todo!");
+  fireEvent.click(button);
+};
 
 it("renders without crashing", function() {
     render(<TodoList />);
