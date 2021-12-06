986722ecfcbcb4b772053895f9077e2549366b31
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-05 23:28:27 -0600

    remove CRA faetures

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 it("renders without crashing", function() {
@@ -9,10 +9,4 @@ it("renders without crashing", function() {
 it("matches snapshot", function() {
   const { asFragment } = render(<App />);
   expect(asFragment()).toMatchSnapshot();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
