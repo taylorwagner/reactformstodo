ebc9c0349cb92020c23398ed00048a7dd77f49e1
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-06 08:50:26 -0600

    additional testing of function

--- src/NewTodoForm.test.js
+++ src/NewTodoForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import NewTodoForm from "./NewTodoForm";
 
 it("renders without crashing", function() {
@@ -9,4 +9,12 @@ it("renders without crashing", function() {
 it("matches snapshot", function() {
     const { asFragment } = render(<NewTodoForm />);
     expect(asFragment()).toMatchSnapshot();
+});
+
+it("runs the create function on form submit", function() {
+  const createMock = jest.fn();
+  const { getByText } = render(<NewTodoForm createTodo={createMock} />);
+  const createButton = getByText("Add a todo!");
+  fireEvent.click(createButton);
+  expect(createMock).toHaveBeenCalled();
 });
